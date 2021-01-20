e8f61e9f76355c16c2bc8845c56d9712c2b3268d
Balpreet Jaggi <balpreet.jaggi@softwareag.com>
2021-01-20 13:23:07 +0100

    Adding Index Page and Controller. Closes #8

---
+++ pet-clinic-web/src/main/java/jaggi/balpreet/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package jaggi.balpreet.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
