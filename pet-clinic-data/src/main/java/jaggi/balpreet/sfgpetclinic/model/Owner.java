c30a2f325bc68355acee87ea2f230db3438a2f19
Balpreet Jaggi <balpreet.jaggi@softwareag.com>
2021-01-29 17:25:10 +0100

    Updated Owners with JPA Entity Annotation. Closes #36

--- pet-clinic-data/src/main/java/jaggi/balpreet/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/jaggi/balpreet/sfgpetclinic/model/Owner.java
@@ -1,13 +1,24 @@
 package jaggi.balpreet.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
-   private Set<Pet> pets = new HashSet<>();
-   private String address;
-   private String city;
-   private String telephone;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets = new HashSet<>();
 
     public Set<Pet> getPets() {
         return pets;
