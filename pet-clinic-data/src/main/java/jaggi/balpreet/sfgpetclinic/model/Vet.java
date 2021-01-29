937f6117d087ad2e165f2492951a553d0f24b35a
Balpreet Jaggi <balpreet.jaggi@softwareag.com>
2021-01-29 17:31:46 +0100

    Updated Vet with JPA Entity Annotation. Closes #37

--- pet-clinic-data/src/main/java/jaggi/balpreet/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/jaggi/balpreet/sfgpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package jaggi.balpreet.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(mappedBy = "vet_id")
+    @JoinTable(name = "vet_specilities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
